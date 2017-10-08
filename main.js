3cc1e764b37bb1e7ffc6ab613326fc8b13268d26
Neo Choi <neo.choi@gmail.com>
2017-10-08 21:51:07 +0800

    update the package

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
-function toChineseNumeral(num){
-	var numerals = {
-		"-":"負",
-		".":"點",
-		0:"零",
-		1:"一",
-		2:"二",
-		3:"三",
-		4:"四",
-		5:"五",
-		6:"六",
-		7:"七",
-		8:"八",
-		9:"九",
-		10:"十",
-		100:"百",
-		1000:"千",
-		10000:"萬",
-		100000000:"億",
-		1000000000000:"兆"
-	};
-	
-	if (num < 0) {
-		return numerals['-'] + toChineseNumeral(-num);
-	} else if (num < 1) {
-		return num.toString().split('').reduce(function(p, n) {
-			return p + numerals[n];
-		}, '');
-	} else if (num > Math.floor(num)) {
-		return toChineseNumeral(Math.floor(num)) + toChineseNumeral(parseFloat(num.toString().replace(/^.*\./, '0.'))).slice(1);
-	} else {
-		return [1000000000000, 100000000, 10000, 1000, 100, 10, 1].reduce(function(p, n) {
-			if (num >= n) {
-				if (p.ling) p.ch += numerals[0];				
-				var t = Math.floor(num / n);
-				p.ch += t < 10 ? numerals[t] : toChineseNumeral(Math.floor(num / n));
-				if (n != 1) p.ch += numerals[n];
-				p.ling = false;
-			} else if (p.ch) {
-				p.ling = true;
-			}
-			num %= n;
-			return p;
-		}, {ch: '', ling: false}).ch.replace(/^一十/, '十');
-	}
+function toChineseNumeral(num) {
+  const numerals = {
+    '-': '負',
+    '.': '點',
+    0: '零',
+    1: '一',
+    2: '二',
+    3: '三',
+    4: '四',
+    5: '五',
+    6: '六',
+    7: '七',
+    8: '八',
+    9: '九',
+    10: '十',
+    100: '百',
+    1000: '千',
+    10000: '萬',
+    100000000: '億',
+    1000000000000: '兆'
+  };
+
+  if (num < 0) {
+    return numerals['-'] + toChineseNumeral(-num);
+  }
+
+  if (num < 1) {
+    return num
+      .toString()
+      .split('')
+      .reduce((p, n) => p + numerals[n], '');
+  }
+
+  if (num > Math.floor(num)) {
+    return (
+      toChineseNumeral(Math.floor(num)) +
+      toChineseNumeral(parseFloat(num.toString().replace(/^.*\./, '0.'))).slice(
+        1
+      )
+    );
+  }
+
+  return [1000000000000, 100000000, 10000, 1000, 100, 10, 1]
+    .reduce(
+      (p, n) => {
+        if (num >= n) {
+          if (p.ling) p.ch += numerals[0];
+          const t = Math.floor(num / n);
+          p.ch += t < 10 ? numerals[t] : toChineseNumeral(Math.floor(num / n));
+          if (n !== 1) p.ch += numerals[n];
+          p.ling = false;
+        } else if (p.ch) {
+          p.ling = true;
+        }
+        num %= n;
+        return p;
+      },
+      { ch: '', ling: false }
+    )
+    .ch.replace(/^一十/, '十');
 }
 
 module.exports.toChineseNumeral = toChineseNumeral;
